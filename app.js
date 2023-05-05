c57da8d2818f7955e3116c324ecf226e7f35167c
Baatar <bsanzhiev@outlook.com>
2023-05-05 22:05:01 +0800

    use mongoose

--- app.js
+++ app.js
@@ -3,15 +3,17 @@
 
 const express = require("express");
 const dotenv = require("dotenv");
-const { MongoClient, ObjectID } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const cors = require("cors");
 
-
 const app = express();
+
 // Work with CORS
 app.use(cors());
+
 // Load environment variables from .env file
 dotenv.config();
+
 // Middleware for parsing request bodies
 app.use(express.json());
 
@@ -20,43 +22,18 @@ const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
 const dbName = "tododb";
 
+const mongoClient = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.get("/", (req, res) => {
-  res.send("This is backend for Todo React Demo App! To get all todos, go to /todos.");
+  res.send(
+    "This is backend for Todo React Demo App! To get all todos, go to /todos."
+  );
 });
 
-// Define a route for creating a new todo
-app.post("/todos", async (req, res) => {
-  try {
-    // Connect to the MongoDB database
-    const client = await MongoClient.connect(uri);
-    const db = client.db(dbName);
-
-    // Create a new todo document
-    const todo = {
-      text: req.body.text,
-      checked: false,
-      createTag: Date.now()
-    };
-
-    // Insert the new todo into the "todos" collection
-    const result = await db.collection("todos").insertOne(todo);
-
-    // Return the new todo object with an assigned ID
-    res.status(201).json({
-      id: result.insertedId,
-      text: todo.text,
-      checked: todo.checked,
-      createTag: todo.createTag,
-    });
-
-    // Close the MongoDB connection
-    client.close();
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Internal server error while creating a new todo" });
-  }
-});
-
+// FETCH ALL TODOS
 // Define a route for retrieving all todos
 app.get("/todos", async (req, res) => {
   try {
@@ -74,10 +51,49 @@ app.get("/todos", async (req, res) => {
     client.close();
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: "Internal server error while retrieving all todos" });
+    res
+      .status(500)
+      .json({ error: "Internal server error while retrieving all todos" });
   }
 });
 
+// CREATE TODO
+// Define a route for creating a new todo
+app.post("/todos", async (req, res) => {
+  try {
+    // Connect to the MongoDB database
+    const client = await MongoClient.connect(uri);
+    const db = client.db(dbName);
+
+    // Create a new todo document
+    const todo = {
+      text: req.body.text, //make it a reqiured field
+      checked: false,
+      createTag: Date.now(),
+    };
+
+    // Insert the new todo into the "todos" collection
+    const result = await db.collection("todos").insertOne(todo);
+
+    // Return the new todo object with an assigned ID
+    res.status(201).json({
+      id: result.insertedId,
+      text: todo.text,
+      checked: todo.checked,
+      createTag: todo.createTag,
+    });
+
+    // Close the MongoDB connection
+    client.close();
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "Internal server error while creating a new todo" });
+  }
+});
+
+// UPDATE TODO
 // Define a route for updating a todo by ID
 app.put("/todos/:id", async (req, res) => {
   try {
@@ -106,10 +122,13 @@ app.put("/todos/:id", async (req, res) => {
     client.close();
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: "Internal server error while updating a todo" });
+    res
+      .status(500)
+      .json({ error: "Internal server error while updating a todo" });
   }
 });
 
+// DELETE TODO
 // Define a route for deleting a todo by ID
 app.delete("/todos/:id", async (req, res) => {
   try {
@@ -117,10 +136,10 @@ app.delete("/todos/:id", async (req, res) => {
     const client = await MongoClient.connect(uri);
     const db = client.db(dbName);
 
-    // Delete the specified todo document
+    // Delete the specified todo
     const result = await db
       .collection("todos")
-      .deleteOne({ _id: ObjectID(req.params.id) });
+      .deleteOne({ _id: ObjectId(req.params.id)});
 
     // If no document was deleted, return a 404 response
     if (result.deletedCount === 0) {
@@ -133,9 +152,50 @@ app.delete("/todos/:id", async (req, res) => {
 
     // Close the MongoDB connection
     client.close();
+
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Internal server error while deleting a todo" });
+  }
+});
+
+// GET TODO BY ID
+// Define a route for retrieving a todo by ID
+app.get("/todos/:id", async (req, res) => {
+  // let query = {_id: ObjectId(req.params.id)};
+  // let todo = await db.collection("todos").findOne(query);
+  // if (!todo) {
+  //   res.status(404).json({ error: "Todo not found" });
+  //   return;
+  // } else res.send(todo).status(200);
+
+  try {
+    // Connect to the MongoDB database
+    const client = await mongoClient.connect();
+    const db = client.db(dbName);
+
+    // Retrieve the specified todo document
+    const todo = await db
+      .collection("todos")
+      .findOne({ _id: ObjectId(req.params.id) });
+
+    // Close the MongoDB connection
+    client.close();
+
+    // If no todo was found, return a 404 response
+    if (!todo) {
+      res.status(404).json({ error: "Todo not found" });
+      return;
+    }
+
+    // Return the retrieved todo
+    res.json(todo);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ error: "Internal server error while deleting a todo" });
+    res
+      .status(500)
+      .json({ error: "Internal server error while retrieving a todo" });
   }
 });
 
---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { ObjectId } = require("mongodb");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+dotenv.config();
+
+// Connect to the MongoDB database
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err));
+
+// Define a schema for todos
+const todoSchema = new mongoose.Schema({
+  text: {type: String, required: true},
+  checked: {type: Boolean, default: false},
+  createTag: {type: Date, default: Date.now}
+},
+{collection: "todos"}
+);
+
+// Define a model for todos
+const Todo = mongoose.model("Todo", todoSchema);
+
+// Index page
+app.get("/", (req, res) => {
+  res.send(
+    "This is backend for Todo React Demo App! To get all todos, go to /todos."
+  );
+});
+
+// FETCH ALL TODOS
+app.get("/todos", async (req, res) => {
+  try {
+    // Retrieve all todos from the "todos" collection
+    const todos = await Todo.find();
+
+    // Return the array of todos
+    res.json(todos);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "Internal server error while retrieving all todos" });
+  }
+});
+
+// CREATE TODO
+app.post("/todos", async (req, res) => {
+  try {
+    // Create a new todo document
+    const todo = new Todo({
+      text: req.body.text,
+    });
+
+    // Save the todo document
+    await todo.save();
+
+    // Return the newly created todo document
+    res.json(todo);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "Internal server error while creating a new todo" });
+  }
+});
+
+// UPDATE TODO
+app.put("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  const {text, checked} = req.body;
+  try {
+    // Find the todo document by its id
+    const todo = await Todo.findOneAndUpdate(
+      id,
+      { text, checked },
+      { new: true }
+    );
+
+    // If the document doesn't exist
+    if (!todo) {
+      return res.status(404).json({ error: "No todo with given id" });
+    }
+
+    // Update the todo document
+    // todo.checked = req.body.checked;
+    // await todo.save();
+
+    // Return success response
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "Internal server error while updating a todo" });
+  }
+});
+
+// DELETE TODO
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    // Find the todo document by its id and delete it
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+
+    // If the document doesn't exist
+    if (!todo) {
+      return res.status(404).json({ error: "No todo with given id" });
+    }
+
+    // Return success response
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ error: "Internal server error while deleting a todo" });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
